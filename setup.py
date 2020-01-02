bbeb0fc7f88dc787576a6e33fb2cb39cea75008a
easyQuant <532894539@qq.com>
2020-01-02 11:14:37 +0800

    支持pypi

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'trade_order',      
+    version = '1.1.0',
+    py_modules = ['trade_order'],
+    author = 'easyQuant',        
+    author_email = '532894539@qq.com',
+    url = 'https://github.com/easyQuant/trade_order',
+    description = 'python版本的 A股实盘交易接口'
+)
